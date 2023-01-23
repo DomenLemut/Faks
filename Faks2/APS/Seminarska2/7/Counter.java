523ca2bea633217104467ed6f363616f16025442
Domen Lemut <dl0300@student.uni-lj.si>
2023-01-23 08:19:12 +0100

    novo

--- Faks2/APS/Seminarska2/7/Counter.java
+++ Faks2/APS/Seminarska2/7/Counter.java
@@ -1,24 +1,11 @@
 class Counter {
     public long startTime = 0;
-    public String lable;
 
-    public long pauseTime = 0;
-
-    public Counter(String lable) {
-        this.lable = lable;
+    public Counter() {
     }
 
     public void Start() {
         startTime = System.nanoTime();
-    }
-
-    public void Pause() {
-        pauseTime = System.nanoTime();
-    }
-
-    public void Resume() {
-        startTime = System.nanoTime() - (pauseTime - startTime);
-        pauseTime = 0;
     }
 
     public void Print() {
